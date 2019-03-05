ccc44e7242082b0fe78c853c2c0d9e032f95c4bd
Romain Torrente <romain.torrente@insa-lyon.fr>
2019-03-05 22:02:19 +0100

    Add crontab

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.7
 EXPOSE 8000
 
-RUN apt-get update && apt-get install -y ghostscript imagemagick libfreetype6
+RUN apt-get update && apt-get install -y ghostscript imagemagick libfreetype6 cron
 
 WORKDIR /app
 COPY requirements.txt /app
--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-yes yes | python3 manage.py migrate && python3 manage.py collectstatic --noinput && gunicorn AffichageDynamique.wsgi -b 0.0.0.0:8000 --log-file -
+yes yes | python3 manage.py migrate && python3 manage.py collectstatic --noinput && python3 manage.py crontab add && gunicorn AffichageDynamique.wsgi -b 0.0.0.0:8000 --log-file -
