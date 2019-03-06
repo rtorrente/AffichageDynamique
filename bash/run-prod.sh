d413a95374e66800fad9a290305ac92470341858
Romain Torrente <romain.torrente@insa-lyon.fr>
2019-03-06 11:44:57 +0100

    Change docker config

--- bash/run-prod.sh
+++ bash/run-prod.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-yes yes | cp scripts/nginx/default.conf /script/nginx/prod && python3 manage.py migrate && python3 manage.py collectstatic --noinput && python3 manage.py crontab add && gunicorn AffichageDynamique.wsgi -b 0.0.0.0:8000 --log-file -
+yes yes | cp -f scripts/nginx/default.conf /script/nginx/prod && python3 manage.py migrate && python3 manage.py collectstatic --noinput && python3 manage.py crontab add && gunicorn AffichageDynamique.wsgi -b 0.0.0.0:8000 --log-file -
