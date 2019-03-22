59185cb06471d0b8fd09274b735c9942884406b2
Romain Torrente <romain.torrente@insa-lyon.fr>
2019-03-22 13:38:50 +0100

    Bug de boloss

--- app/models.py
+++ app/models.py
@@ -212,7 +212,7 @@ class Subscription(models.Model):
 
 class Content(models.Model):
     name = models.CharField(verbose_name='Nom du contenu', blank=False, max_length=255, null=False)
-    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
+    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1, editable=False)
     user_moderator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="+", blank=True)
     reject_reason = models.TextField(null=True, default=None, blank=True)
     begin_date = models.DateTimeField(verbose_name="Début d'affichage", blank=False, null=False)
